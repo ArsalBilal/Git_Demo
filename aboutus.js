17b978273ab124a38d34270b1fd6fcaa53c8fdc8
Arsal Bilal <jountyroud@gmail.com>
2025-07-29 14:44:46 +0500

    Add About us page

---
+++ aboutus.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function AboutUs() {
+  return (
+    <div className="aboutus-container" style={{ padding: '1rem', maxWidth: '800px', margin: 'auto', fontFamily: 'Arial, sans-serif' }}>
+      <h1>About Us</h1>
+      <p>
+        Welcome to our application. We are dedicated to providing the best user experience through our innovative solutions.
+      </p>
+      <p>
+        Our team is passionate about technology and committed to continuous improvement and customer satisfaction.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
